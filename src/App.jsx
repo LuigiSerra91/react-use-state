d7e1f5f780a424a8e0e1325f57158d9eff9aa038
LuigiSerra91 <luigi_serra91@hotmail.it>
2024-11-15 14:23:05 +0100

    import_data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
+import data from './data/languages.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [active, setActive] = useState(0)
+
+  function handleClick(e){
+    console.log(active);
+    const newActive = Number(e.target.getAttribute('data-index'))
+    console.log(newActive);
+    setActive(newActive)
+
+  }
 
   return (
     <>
