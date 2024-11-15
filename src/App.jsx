025a3b0d5811d4d87c9b471b9604e9f1107292dc
LuigiSerra91 <luigi_serra91@hotmail.it>
2024-11-15 15:50:31 +0100

    add_map_btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,41 @@
 import { useState } from 'react'
 import data from './data/languages.js'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 
-function App() {
+const App = () => {
+  // Stato per il linguaggio selezionato
+  const [active, setActive] = useState(data[0]);
 
-  const [active, setActive] = useState(0)
-
-  function handleClick(e){
-    console.log(active);
-    const newActive = Number(e.target.getAttribute('data-index'))
-    console.log(newActive);
-    setActive(newActive)
-
-  }
+  // Funzione per gestire il click sui bottoni
+  const handleClick = (post) => {
+    setActive(post);
+  };
 
   return (
-    <>
+    <div>
+      <h1>Learn Web development</h1>
       
-    </>
-  )
+      {/* Lista di bottoni per ogni linguaggio */}
+      <div>
+        {data.map((post) => (
+          
+            <button className='btn' key={post.id} onClick={() => handleClick(post)}>
+            {post.title}
+          </button>
+          
+          
+        ))}
+      </div>
+
+      {/* Card con il linguaggio selezionato */}
+      <div className='card'>
+        <h2>{active.title}</h2>
+        <p>{active.description}</p>
+      </div>
+    </div>
+  );
 }
+
 
 export default App
